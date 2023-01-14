78f9d820cf5a103e83de0edb6712aac469d66ff3
Mark <yaazzik@gmail.com>
2023-01-14 21:58:41 +0300

    Глебов Марк все починил

--- script.test.js
+++ script.test.js
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test'
 import { phrases } from './phrases.js'
 
-test.describe('Tests of types', () => {
-    test('Phrases должен иметь тип string', () => {
-        phrases.map((phrase) => {
-            expect(phrase).toBeInstanceOf(String)
-        })
+test('Phrases должен иметь тип string', () => {
+    phrases.map((phrase) => {
+        expect(phrase).toBeInstanceOf(String)
     })
 })
